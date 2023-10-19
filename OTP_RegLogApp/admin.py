5e673049b57806883354e104aea4e652d5953764
MallamDileepKumar <mallamdileep@gmail.com>
2023-10-19 11:47:38 +0530

    3rd commit

--- OTP_RegLogApp/admin.py
+++ OTP_RegLogApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    List_display = ['username','email','password','conf_password','mobile']
+    List_filter = ['username']
+
+admin.site.register(User,UserAdmin)
